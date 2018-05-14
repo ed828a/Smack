416a348c697220cbdc26ae4938072a8928f0887a
Edward <ed828a@gmail.com>
2018-05-14 14:56:09 +0930

    create Create User Activity

---
+++ app/src/main/java/com/example/edward/smack/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.example.edward.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+     fun onGenerateUserAvatar(view: View){
+
+     }
+
+    fun onGenerateBackgroundColorClick(view: View){
+
+    }
+
+    fun onCreateUserClick(view: View){
+
+    }
+}
--- app/src/main/java/com/example/edward/smack/LoginActivity.kt
+++ app/src/main/java/com/example/edward/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.edward.smack
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,6 +17,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun onLoginCreateUserButtonClick(view: View){
-
+        val intent = Intent(this, CreateUserActivity::class.java)
+        startActivity(intent)
     }
 }
